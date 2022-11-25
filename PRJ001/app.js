9921cc443e75bf853c3b79d95c04081fac98e74f
Sanjeet Dutt <sanjeetdutt@gmail.com>
2022-11-25 16:16:16 +0530

    Adding sequelize and its config

--- PRJ001/app.js
+++ PRJ001/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser")
 // Custom imports
 const path = require("./src/util/path")
 const myRouter = require("./src/routes/main")
+const sequelize = require("./src/util/database")
 
 // Creating new application
 const app = express()
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join("public")))
 app.use(express.json())
 
+// adding router to the application
 myRouter(app)
 
 
@@ -22,5 +24,10 @@ app.use((req,res)=>{
 })
 
 // Starting Application
-console.log("APPLICATION IS RUNNING ON PORT 3000")
-app.listen(3000)
+
+sequelize.sync().then(()=>{
+    console.log("server started on port 3000")
+    app.listen(3000)
+}).catch(err=>{
+    console.error("Error while starting the application " , err)
+})
